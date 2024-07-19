c65b65dd17afdd2a16aa9e8ce95528cb6e8d83dc
Kohei Matsukawa <kohei.mattsukawa@gmail.com>
2024-07-19 13:01:35 +0900

    [add] ライブラリが確保する予備の表示領域を非表示にする

--- src/book-search/app/search/barcode/page.tsx
+++ src/book-search/app/search/barcode/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import styles from "./page.module.css";
 import { quaggaStart, quaggaStop } from "@/app/lib/quaggaApi";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
@@ -7,6 +8,25 @@ import { useEffect, useRef, useState } from "react";
 export default function Barcode() {
   const router = useRouter();
 
+  // TODO: この処理はlibへ移した方がよさそう
+  const sleep = (milliSecond: number) => new Promise(resolve => setTimeout(resolve, milliSecond));
+
+  /** 1秒あればメディアの表示が完了すると思いたい */
+  const sleepMilliSecond = 1000;
+
+  /** ライブラリが予備で確保する表示領域を非表示にする */
+  const displayNoneCanvas = () => {
+    const ele = document.getElementById("interactive");
+    const canvasEleArray = ele?.getElementsByTagName('canvas') || [];
+    console.log(canvasEleArray);
+    if (!canvasEleArray.length || !canvasEleArray[0]) {
+      return;
+    }
+
+    canvasEleArray[0].classList.add(styles.displayNone);
+  }
+
+  /** カメラを切る */
   const stopMedia = () => {
     const ele = document.getElementById("interactive");
     const videoEleArray = ele?.getElementsByTagName('video') || [];
@@ -20,6 +40,7 @@ export default function Barcode() {
     quaggaStop();
   }
 
+  /** 初回レンダリングで呼ばれる */
   const lock = useRef(false);
   useEffect(() => {
     if (lock.current) {
@@ -27,6 +48,13 @@ export default function Barcode() {
     }
     
     const canMedia = quaggaStart();
+    if (canMedia) {
+      /** ライブラリが予備で確保する表示領域を非表示にする */
+      (async () => {
+        await sleep(sleepMilliSecond);
+        displayNoneCanvas();
+      })();
+    }
     lock.current = true;
     // TODO: カメラ取得失敗時はエラーメッセージを出す
 
@@ -35,12 +63,12 @@ export default function Barcode() {
     };
   }, []);
 
+  /** 戻るボタン */
   const onClickBack = () => {
     stopMedia();
     router.push('/search');
   }
   
-  // TODO: 余分な要素を非表示にする
   return (
     <div>
       <div id="interactive" className="viewport"></div>
